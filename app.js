2ba85423a0b1a24bc8bb21ae6fee02eedced33db
sobunkohla <sobunkohla@gmail.com>
2023-01-28 20:01:35 +0200

    for loop

---
+++ app.js
@@ -0,0 +1,94 @@
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32;
+console.log(fahrenheit);
+
+
+let bool =  !false ;
+console.log(bool)
+
+let subscribed = false;
+let loggedIn = true
+if (subscribed === true) {
+    console.log('show the video');
+}
+else if (loggedIn === true) {
+ console.log( 'tell the user to upgrade ')
+}
+else {
+    console.log ('tell user to log into an account');
+}
+
+
+let cash = 50
+let price = 60
+let difference = cash - price;
+if (cash > price) {
+    console.log(`you payed extra money here's your ${difference} dollar change`)
+}
+else if (cash === price) {
+    console.log("you payed the exact amount have a nice day")
+}
+else  {
+ console.log(`not enough money-  you still owe ${difference * -1} dollars`);
+}
+
+let cash2 = 50;
+let price2 = 40;
+let isStoreOpen = false;
+
+if ( cash2 >= price2 && !isStoreOpen) {
+ console.log('print recipt')
+}
+
+let val = "falsey"
+ 
+if (val) {
+    console.log(val);
+} else {
+    console.log(!!val);
+}
+
+let fire = true;
+
+fire ? console.log('hot') : console.log('cold');
+
+let subscribe = false ;
+let loggIn = true ;
+
+let str = subscribe && loggIn ? "show video" : "hide video";
+console.log(str);
+
+let cash3 = 50;
+let price3 = 40;
+let openStore = false;
+
+let recipt = cash3 >= price3 && openStore ? "Give recipt" : "Don't give recipt";
+console.log(recipt);
+
+// let count = 1
+// // while (count <= 100) {
+// //     console.log(count);
+// //     count ++;
+    
+// // }
+
+for (let i = 1; i <=100; i++ ) {
+    console.log(i)
+}
+
+for (let i = 1; i<= 20; i++) {
+    
+    if (i % 3 ===0 && i %5 ===0 ) {
+        console.log("frontend simplified")
+     }
+   else if (i % 3 ===0) {
+        console.log("frontend")
+    }
+    else if (i %5 ===0) {
+        console.log("Simplified")
+    }
+    else if (i % 3 !==0 || i %5 !==0) {
+        console.log(i)
+    }
+    
+}
